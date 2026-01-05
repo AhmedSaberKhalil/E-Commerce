d675d4193e71a2801627c9ee81a85b195c3fc975
Ahmed Khalil <ahmedsaberacu77@gmail.com>
2026-01-05 05:08:21 +0200

    Add Column attribute to Price property in Product model

--- E-CommerceWebApi/Models/Product.cs
+++ E-CommerceWebApi/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace E_CommerceWebApi.Models
@@ -10,6 +11,8 @@ namespace E_CommerceWebApi.Models
         [StringLength(15, ErrorMessage = "Product name cannot exceed 15 characters.")]
         public string Name { get; set; }
         [Range(0.01, 999999.99, ErrorMessage = "Price must be between 0.01 and 999999.99.")]
+        [Column(TypeName = "decimal(18,2)")]
+
         public decimal Price { get; set; }
         [StringLength(100, ErrorMessage = "Description cannot exceed 100 characters.")]
         public string Description { get; set; }
