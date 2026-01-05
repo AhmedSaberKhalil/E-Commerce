f4b4e45a594cee0185cb67bfe45901eb5920c52e
Ahmed Khalil <ahmedsaberacu77@gmail.com>
2026-01-05 02:06:00 +0200

    add a dummy connectionString

---
+++ E-CommerceWebApi/Data/ECEntityFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace E_CommerceWebApi.Data
+{
+    public class ECEntityFactory : IDesignTimeDbContextFactory<ECEntity>
+    {
+        public ECEntity CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ECEntity>();
+
+            // This connection string is a dummy! 
+            // It only tells EF "I am using SQL Server" so it can generate the right SQL syntax.
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Dummy;User Id=sa;Password=Password;TrustServerCertificate=True;");
+
+            return new ECEntity(optionsBuilder.Options);
+        }
+    }
+}
